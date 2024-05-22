2acab89cf8b474d60f09298a72de9fa3e854041f
Sergey Volkov <rusobraz@mail.ru>
2024-05-22 20:42:46 +0300

    Решение ТЗ№2 Дополнил READM

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -13,19 +13,22 @@ public class Main {
         List<Filter> filters = List.of(filterTwoHours, filterArrival, filterFlightBefore);
         Filter allFilters = new AllTestFilters(filters);
 
-        System.out.println("=".repeat(50));
+        System.out.println("Все перелеты");
+        flights.forEach(System.out::println);
+
+        System.out.println("Исключение перелетов где общее время, проведённое на земле, превышает два часа");
         FilterFlightManager flightManager = new FilterFlightManager(filterTwoHours);
         flightManager.getFilterFlight(flights).forEach(System.out::println);
 
-        System.out.println("=".repeat(50));
+        System.out.println("Исключение перелетов где сегменты с датой прилёта позже даты вылета.");
         flightManager = new FilterFlightManager(filterArrival);
         flightManager.getFilterFlight(flights).forEach(System.out::println);
 
-        System.out.println("=".repeat(50));
+        System.out.println("Исключение перелетов до текущего момента времени.");
         flightManager = new FilterFlightManager(filterFlightBefore);
         flightManager.getFilterFlight(flights).forEach(System.out::println);
 
-        System.out.println("=".repeat(50));
+        System.out.println("Фильтрация по всем правилам");
         flightManager = new FilterFlightManager(allFilters);
         flightManager.getFilterFlight(flights).forEach(System.out::println);
     }
